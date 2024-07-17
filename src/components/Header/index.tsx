7542f099cee5857380790a9c60fc49ca9abbf41b
Kane Ma <kanema20@gmail.com>
2024-07-16 23:29:47 -0400

    get account balance

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@
 import useMediaSize from "@/hooks/useMediaSize";
 import { useAutoConnect } from "../../app/AutoConnectProvider";
 import { WalletSelector as ShadcnWalletSelector } from "../../app/WalletSelector";
+import { Button } from "../../app/ui/button";
 
 import { ClientOnly } from "../ui/client-only";
 import {
@@ -10,13 +11,42 @@ import {
     NetworkInfo,
     WalletInfo,
     isAptosNetwork,
-    useWallet,
+    useWallet
   } from "@aptos-labs/wallet-adapter-react";
+  import { Account, Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+import { useState, useEffect } from "react";
+
 
 export function Header() {
     const { account, connected, network, wallet, changeNetwork } = useWallet();
     const screenWidth = useMediaSize();
     const isMobile = screenWidth < 768;
+
+    const [userBalance, setUserBalance] = useState<number | null>(null);
+
+    const aptosConfig = new AptosConfig({ network: Network.DEVNET });
+    const aptos = new Aptos(aptosConfig);
+
+    const userAddress = account?.address;
+    console.log("userAddress: ", userAddress)
+
+    const getAptBalance = async () => {
+        type Coin = { coin: { value: string } };
+ 
+        const resource = await aptos.getAccountResource<Coin>({
+            accountAddress: userAddress!,
+            resourceType: "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>",
+        });
+        const bal = (parseInt(resource.coin.value) / 100000000).toFixed(2);
+        console.log("bal: ", bal)   
+        setUserBalance(parseInt(bal));
+    }
+
+    useEffect(() => {
+        if (userAddress) {
+            getAptBalance();
+        }
+    }, [userAddress])
     
     let Links =[
         {name:"Play",link:"/games/blackjack"},
@@ -41,10 +71,13 @@ export function Header() {
                         <a href={link.link} className='text-gray-800 hover:text-blue-400 duration-500'>{link.name}</a>
                     </li>))
                 }
-                <div className="flex flex-col gap-4 items-center pl-8">
-            {/* <div className="text-sm text-muted-foreground">shadcn/ui</div> */}
-            <ShadcnWalletSelector />
-          </div>
+            <div className="flex flex-col gap-4 items-center pl-4">
+                {userAddress ? <Button>{userBalance} APT</Button> : <></>}
+            </div>
+            <div className="flex flex-col gap-4 items-center pl-4">
+                {/* <div className="text-sm text-muted-foreground">shadcn/ui</div> */}
+                <ShadcnWalletSelector />
+            </div>
              {/* <button className='btn bg-blue-600 text-white md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static'>Connect Wallet</button> */}
          </ul>
          {/* button */}
